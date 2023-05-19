273033dfc202d2df1f68adceea1dfce67edc844e
VoMinhLoi <minhloi1131130@gmail.com>
2023-05-19 16:09:59 +0700

    Tải ảnh lên ImageView bằng cách dùng Intent bằng Action_Get_Content để vào Gallery lấy ảnh và dùng Intent để GetData ảnh đã chọn rồi tải lên ImageView.

--- app/src/main/java/com/example/learnbymyself/Activity/Activity/ProfileActivity.java
+++ app/src/main/java/com/example/learnbymyself/Activity/Activity/ProfileActivity.java
@@ -1,28 +1,112 @@
 package com.example.learnbymyself.Activity.Activity;
 
-import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.widget.ImageView;
+import static android.content.ContentValues.TAG;
 
-import androidx.annotation.Nullable;
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.bumptech.glide.Glide;
 import com.example.learnbymyself.R;
 
+import java.io.IOException;
+
 public class ProfileActivity extends AppCompatActivity {
+
+    private static final int MY_REQUEST_CODE = 10;
     ImageView avatarProfile;
+    TextView loadImgTV;
+    Button saveBT;
+    private ActivityResultLauncher<Intent> mActivityResultLaucher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    Log.e(TAG, "onActivityResult");
+                    if(result.getResultCode() == Activity.RESULT_OK){
+                        Intent data = result.getData();
+                        if(data == null){
+                            return;
+                        }
+                        Uri uri = data.getData();
+                        try {
+                            Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+
+                            avatarProfile.setImageBitmap(bitmap);
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+
+                    }
+                }
+            }
+    );
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(Bundle savedInstanceState)  {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         AnhXa();
-//        Glide.with(this)
-//                .load(R.drawable.img)
-//                .circleCrop()
-//                .into(avatarProfile);
+        loadImgTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                onClickRequestPermission();
+                openGallery();
+                System.out.println("Cos lang nghe");
+            }
+        });
     }
     public void AnhXa(){
+        saveBT = findViewById(R.id.Profile_saveBT);
+        loadImgTV = findViewById(R.id.Profile_loadImgTV);
         avatarProfile = findViewById(R.id.Profile_Avatar);
+    }
+//    private void onClickRequestPermission() {
+//        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.M){
+//            openGallery();
+//            return;
+//        }if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+//            openGallery();
+//        }else{
+//            String [] permission = {Manifest.permission.READ_EXTERNAL_STORAGE};
+//            requestPermissions(permission, MY_REQUEST_CODE);
+//        }
+//    }
+
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//        System.out.println(requestCode);
+//        System.out.println(grantResults.length);
+//        System.out.println(grantResults[0]);
+//
+//        if(requestCode == MY_REQUEST_CODE){
+//            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+//                openGallery();
+//            }
+//        }
+//    }
+
+    private void openGallery() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        mActivityResultLaucher.launch(Intent.createChooser(intent,"Select Picture"));
     }
 }
